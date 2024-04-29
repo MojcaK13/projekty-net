6adf6b352cc9d9d2f302c3815002c5d34d3eb0d3
Marlena Kurczab <marlena.mojca@gmail.com>
2024-04-29 18:29:25 +0200

    wszystko.

---
+++ Linuxoteka/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Linuxoteka
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst = "C:\\Users\\mojca\\Desktop\\projekty-net\\Linuxoteka\\UbuntuLista.txt";
+            if (File.Exists(tekst))
+            {
+                string zawartosc = File.ReadAllText(tekst);
+                Console.WriteLine("Zawartosc pliku: ");
+                Console.WriteLine(zawartosc);
+            }
+            else
+            {
+                Console.WriteLine("brak pliku!");
+            }
+        }
+    }
+}
